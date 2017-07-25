a2817965580c18b6cc8389ffa5018d06326bf0d7
Kishore <kishore.iiitn@gmail.com>
2017-07-25 21:00:17 +0530

    Add onSubmit to FormLayout.

--- ts/Components/Formlayout/index.tsx
+++ ts/Components/Formlayout/index.tsx
@@ -4,14 +4,15 @@ import {Form} from '../Form';
 import * as PropTypes from 'prop-types';
 
 export interface IProps {
-	label: string
+	label: string,
+	onSubmit?:Function
 };
 export interface IState {};
 
 export class Formlayout extends React.Component<IProps, IState> {
 
 	render() {
-		return <Form cls="form-layout">
+		return <Form cls="form-layout" onSubmit={this.props.onSubmit}>
 			<h3 style={{marginBottom: 10}}>{this.props.label}</h3>
 			{this.props.children}
 		</Form>;
