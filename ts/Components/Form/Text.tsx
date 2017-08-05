d882ed95e8e21578f3279b05bdcd95c9cf96d2a4
Kishore <kishore.iiitn@gmail.com>
2017-08-05 22:35:17 +0530

    Add Validation to Text Component.

--- ts/Components/Form/Text.tsx
+++ ts/Components/Form/Text.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 
 export interface ITextProps {
 	name: string,
-	send_value?: Function,
 	autoFocus?: boolean
-	_classui_form_capture?: boolean
+	type?: "number" | "text" | "url" | "email" | RegExp | "inlist"
+	inList?: string[],
+	minSize?: number,
+	maxSize?: number
+
+	send_value?: Function
+	report?: Function
 };
 
 export class Text extends React.Component<ITextProps, any> {
@@ -12,7 +18,7 @@ export class Text extends React.Component<ITextProps, any> {
 	private input: HTMLInputElement | null;
 	static defaultProps = {
 		autoFocus: false,
-		_classui_form_capture: true
+		type: "text"
 	};
 
 	constructor() {
@@ -28,13 +34,41 @@ export class Text extends React.Component<ITextProps, any> {
 		if (this.props.send_value){
 			let json = {
 				name: this.props.name,
-				value: val
+				value: val,
+				error: this.validate(val)
 			};
 			this.props.send_value(json);
 		};
 	}
 	sendToForm(e: React.ChangeEvent<HTMLInputElement>) {
+		if (this.props.report) {
+			this.props.report({
+				error: this.validate(e.target.value)
+			});
+		}
 		this.send(e.target.value);
+	}
+
+	validate(text: string): string | null {
+		switch(this.props.type) {
+			case "email": {
+				let regex = "";
+				break;
+			}
+			case "number": {
+				break;
+			}
+			case "inlist": {
+				if (!_.includes(this.props.inList, text)){
+					return `Should be one of (${this.props.inList})`;
+				}
+				break;
+			}
+		}
+		if ((this.props.minSize && text.length<this.props.minSize) || (this.props.maxSize && text.length>this.props.maxSize)) {
+			return `Len : (${this.props.minSize?this.props.minSize:0} and ${this.props.maxSize?this.props.maxSize:'any'})`;
+		}
+		return null;
 	}
 
 	componentDidMount() {
@@ -44,6 +78,14 @@ export class Text extends React.Component<ITextProps, any> {
 	}
 
 	render() {
-		return <input type="text" autoFocus={this.props.autoFocus} autoComplete="off" ref={(ref)=>{this.input=ref}} spellCheck={false} name={this.props.name} placeholder="Enter text" onChange={this.sendToForm}/>
+		return <input type="text" 
+			autoFocus={this.props.autoFocus}
+			autoComplete="off"
+			ref={(ref)=>{this.input=ref}}
+			spellCheck={false}
+			name={this.props.name}
+			placeholder="Enter a value"
+			onChange={this.sendToForm}
+		/>;
 	}
 };
