33830cc2b8d49c917315d9619bc35fc149e9290c
Kishore <kishore.iiitn@gmail.com>
2017-08-05 22:36:01 +0530

    Add Error handling to TextField.

--- ts/Components/Formlayout/TextField.tsx
+++ ts/Components/Formlayout/TextField.tsx
@@ -2,17 +2,29 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Text, ITextProps} from '../Form/Text';
 import {FormHOC} from '../Form';
+import {SAnim} from '../../Helper/Animation';
+import * as classNames from 'classnames';
 
 export interface IProps extends ITextProps {
 	children: string,
 	name: string
 };
 
-class _TextField extends React.Component<IProps, {}> {
+class _TextField extends React.Component<IProps, {error: null|string}> {
+	constructor() {
+		super();
+		this.state = {
+			error: null
+		};
+	}
 	render() {
-		return <label className="formelement">
+		let cls = classNames("formelement", {
+			"error": this.state.error?true:false
+		});
+		return <label className={cls}>
 			<div className="label">{this.props.children}</div>
-			<Text {...this.props}></Text>
+			<div className="error">{this.state.error}</div>
+			<Text report={(json:any)=>{this.setState({error: json.error})}} {...this.props}>Enter {this.props.children}</Text>
 		</label>;
 	}
 };
