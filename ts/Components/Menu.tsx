37a65018bc5a8e94eadb4e22f58c0209a147f579
Kishore <kishore.iiitn@gmail.com>
2017-09-15 17:13:11 +0530

    Rename SideMenu to more generic Menu.

---
+++ ts/Components/Menu.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as classNames from 'classnames';
+import {TransitionMotion} from 'react-motion';
+import {MAnim} from '../Helper/Animation';
+
+export interface IProps {
+	header?: string
+	children: any
+};
+export interface IState {};
+
+export class Menu extends React.Component<IProps, IState> {
+	render() {
+		return <TransitionMotion {...MAnim.opacity(this.props.children)}>{
+			(styles)=>{
+				return <div className="sidemenu">{
+					this.props.header?<h3 className="header">{this.props.header}</h3>:null
+				}{this.props.children}</div>;
+		}}</TransitionMotion>;
+	}
+}
+
+
+export type IItemProps = {
+	disable?: boolean
+	active?: boolean
+	children: any
+	style?: React.CSSProperties
+	badge?: string
+};
+
+export let Divider = ()=> {
+	return <div className="divider"></div>;
+}
+
+export let Item = (props: IItemProps)=>{
+	let cls = classNames("item", {
+		disable: props.disable,
+		active: props.active
+	});
+	return <div className={cls} style={props.style}>
+		{props.children} {props.badge?<span className="badge">{props.badge}</span>:null}
+	</div>;
+}
