c4a61a23929f5dfd4704cf4a50fdbedeeedf0fbb
Kishore <kishore.iiitn@gmail.com>
2017-09-20 21:14:13 +0530

    Add no dismissible prop to Flash component and add z-index to navbar.

--- ts/Components/Flash.tsx
+++ ts/Components/Flash.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {SAnim} from '../Helper/Animation';
 
-export interface IProps {};
+export interface IProps {
+	nodismiss?: boolean
+};
 export interface IState {
 	show: boolean
 };
@@ -10,14 +12,16 @@ export interface IState {
 let _instance: Flash | null = null;
 export class Flash extends React.Component<IProps, IState> {
 
+	private noDismiss = false;
 	private content_click: boolean = false;
 	private content: any;
 
-	static flash(func: Function) {
+	static flash(func: (dismiss: any)=>any, noDismiss: boolean = false) {
 		if (!_instance){
 			console.error("Flash component should be rendered to use it.");
 			return;
 		}
+		_instance.noDismiss = noDismiss;
 		_instance.content = func(_instance? _instance.dismiss: null);
 		_instance.setState({
 			show: true
@@ -35,25 +39,29 @@ export class Flash extends React.Component<IProps, IState> {
 			show: false
 		};
 		this.escapeDismiss = this.escapeDismiss.bind(this);
+		this.clickDismiss = this.clickDismiss.bind(this);
 		this.dismiss = this.dismiss.bind(this);
 	}
 
 	escapeDismiss(e: KeyboardEvent) {
-		if (e.key=="Escape") {
+		if (e.key=="Escape" && !this.noDismiss) {
 			this.dismiss();
 			window.removeEventListener("keydown", this.escapeDismiss);
 		}
 	}
-	dismiss() {
-		if (this.content_click){
+	clickDismiss() {
+		if (this.content_click && !this.noDismiss){
 			this.content_click = false;
 			return;
 		}
+		this.dismiss();
+	}
+	dismiss() {
 		this.setState({show: false});
 	}
 	render() {
 		return <SAnim show={this.state.show}>
-			<div className="flash" onClick={this.dismiss}>
+			<div className="flash" onClick={this.clickDismiss}>
 				<div onClick={(e)=>{this.content_click=true}} className="content card-5">
 					{this.content}
 				</div>
