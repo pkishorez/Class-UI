496edb587e91ea1fa150cdcd07ac06a5a62b4698
Kishore <kishore.iiitn@gmail.com>
2017-11-26 10:30:25 +0530

    Remove state from OrderedMapList component to facilitate updating through props only.
    Include DataStructures folder to include most commonly used DataStructures.

--- ts/Components/OrderedMapList.tsx
+++ ts/Components/OrderedMapList.tsx
@@ -2,77 +2,71 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as _ from 'lodash';
 import { Layout, Section } from './Layout';
+import {IOrderedMap} from '../DataStructures/OrderedMap';
 
 export interface IProps {
-	order: {
-		key: string
-		title: string
-	}[]
+	orderedMap: IOrderedMap<any>
 
 	onClick?: (id: string)=>void
 	canDelete?: boolean
 	onDelete?: (id: string)=>void
+	onOrderChange?: (order: string[])=>void
 };
-export interface IState {
-	order: IProps['order']
-}
 
-export class OrderedMapList extends React.Component<IProps, IState> {
+export class OrderedMapList extends React.Component<IProps> {
 	static defaultProps: Partial<IProps> = {
 		canDelete: true
 	}
 	constructor(props: IProps, context: any) {
 		super(props, context);
-		this.getOrder = this.getOrder.bind(this);
 		this.moveDown = this.moveDown.bind(this);
 		this.moveUp = this.moveUp.bind(this);
-		this.state = {
-			order: props.order
-		};
-	}
-	getOrder() {
-		return _.map(this.state.order, "key")
 	}
 	private moveDown(index: number) {
-		let order = _.cloneDeep(this.state.order);
-		let temp = _.cloneDeep(order[index]);
+		let order = _.cloneDeep(this.props.orderedMap.order);
+		let temp = order[index];
 		order[index] = order[index+1];
 		order[index+1] = temp;
-		this.setState({order});
+		if (this.props.onOrderChange) {
+			this.props.onOrderChange(order);
+		}
 	}
 	private moveUp(index: number) {
-		let order = _.cloneDeep(this.state.order);
-		let temp = _.cloneDeep(order[index]);
+		let order = _.cloneDeep(this.props.orderedMap.order);
+		let temp = order[index];
 		order[index] = order[index-1];
 		order[index-1] = temp;
-		this.setState({order});
+		if (this.props.onOrderChange) {
+			this.props.onOrderChange(order);
+		}
 	}
 	render() {
-		let order = this.state.order.map((item, i)=>{
+		let order = this.props.orderedMap.order.map((item_id, i)=>{
 			let index = i;
+			let mappedItem = this.props.orderedMap.map[item_id];
 			let up = <span className="button" style={{
 				visibility: (index!=0)?"visible":"hidden"
 			}} onClick={(e)=>{this.moveUp(index); e.stopPropagation()}}><i className="fa fa-long-arrow-up"></i></span>;
 
 			let down = <span className="button" style={{
-				visibility: (index!=this.state.order.length-1)?"visible":"hidden"
+				visibility: (index!=this.props.orderedMap.order.length-1)?"visible":"hidden"
 			}} onClick={(e)=>{this.moveDown(index); e.stopPropagation()}}>
 				<i className="fa fa-long-arrow-down"></i>
 			</span>;
 
 			return <div className="item" onClick={(e)=>{
-				this.props.onClick?this.props.onClick(item.key):null;
+				this.props.onClick?this.props.onClick(item_id):null;
 			}}>
-				<Layout key={item.key} align="center" style={{height: 30}}>
+				<Layout key={item_id} align="center" style={{height: 30}}>
 					<Section>
-						{item.title}
+						{mappedItem.title}
 					</Section>
 					<Section remain />
 					<Section>
 						{up}
 						{down}
 						{this.props.canDelete?<span style={{marginLeft: 10}} className="button primary" onClick={(e)=>{
-							this.props.onDelete?this.props.onDelete(item.key):null
+							this.props.onDelete?this.props.onDelete(item_id):null
 							e.stopPropagation();
 						}}>
 							<i className="fa fa-trash"></i>
