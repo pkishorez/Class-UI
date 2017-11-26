bdf037cfb9850be5998e50e66581eb94b18df1b8
Kishore <kishore.iiitn@gmail.com>
2017-11-26 11:21:42 +0530

    Include testpublish used for installing package locally without publishing to npm.

---
+++ testpublish.sh
@@ -0,0 +1,34 @@
+# !/bin/sh
+
+rm -r ./Components
+rm -r ./DataStructures
+rm -r ./Helper
+rm -r ./docs
+rm ./ClassUI.d.ts
+rm ./ClassUI.js
+rm ./Navbar.d.ts
+rm ./Navbar.js
+rm ./Content.d.ts
+rm ./Content.js
+rm ./classui.tgz
+
+tsc
+
+npm pack
+
+mv classui-* classui.tgz
+cd ~/Dream/IIITN/
+npm install
+cd ~/Dream/Class-UI/
+
+rm -r ./Components
+rm -r ./DataStructures
+rm -r ./Helper
+rm -r ./docs
+rm ./ClassUI.d.ts
+rm ./ClassUI.js
+rm ./Navbar.d.ts
+rm ./Navbar.js
+rm ./Content.d.ts
+rm ./Content.js
+rm ./classui.tgz
