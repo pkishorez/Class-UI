31e59424a36ebd7414bd79cf612a67be79f784f4
Kishore <kishore.iiitn@gmail.com>
2017-12-20 10:48:16 +0530

    Add groupBy functionality to Table component.

--- ts/Components/Table.tsx
+++ ts/Components/Table.tsx
@@ -3,15 +3,17 @@ import * as ReactDOM from 'react-dom';
 import * as classNames from 'classnames';
 import { Layout, Section } from './Layout';
 import * as _ from 'lodash';
+import { Menu } from './Menu';
 
 export interface IProps {
 	hoverable?: boolean
 
 	headerItems: string[],
 	sortableItems?: string[]
-	groupableItems?: string[] //Yet to be implemented!!!
+	groupableItems?: string[]
 
 	data: any[],
+	rowOnClick?: (rowdata: any)=>void
 	columnUI?: {
 		[id: string]: (value: any)=>any
 	}
@@ -21,12 +23,17 @@ export interface IState {
 		by: string
 		order: "asc"|"desc"
 	}
-	groupby: string
+	group: {
+		by?: string
+		value?: any
+	}
 };
 
 export class Table extends React.Component<IProps, IState> {
 	static defaultProps: Partial<IProps> = {
-		hoverable: false
+		hoverable: false,
+		sortableItems: [],
+		groupableItems: []
 	};
 
 	constructor(props: IProps, context: any) {
@@ -34,7 +41,10 @@ export class Table extends React.Component<IProps, IState> {
 
 		this.state = {
 			sort: {by: "", order: "asc"},
-			groupby: ""
+			group: {
+				by: undefined,
+				value: undefined
+			}
 		};
 		this.groupBy = this.groupBy.bind(this);
 		this.getItemsByCriteria = this.getItemsByCriteria.bind(this);
@@ -42,16 +52,25 @@ export class Table extends React.Component<IProps, IState> {
 	}
 
 	getItemsByCriteria(): any[] {
+		let data = this.props.data;
 		if (this.state.sort.by!=""){
 			let sortBy = (row: any)=>_.isString(row[this.state.sort.by])?row[this.state.sort.by].toLowerCase():row[this.state.sort.by];
-			return _.orderBy(this.props.data, 
+			data = _.orderBy(data, 
 				[sortBy],
 			this.state.sort.order);
 		}
-		return this.props.data;
+		if (this.state.group.by && this.state.group.value) {
+			data = _.groupBy(data, this.state.group.by)[this.state.group.value];
+		}
+		return data;
 	}
-	groupBy(key: string) {
-
+	groupBy(item: string, value?: string) {
+		this.setState({
+			group: {
+				by: item,
+				value
+			}
+		});
 	}
 	sortBy(key: string) {
 		if (_.includes(this.props.sortableItems, key)) {
@@ -68,17 +87,14 @@ export class Table extends React.Component<IProps, IState> {
 		}
 	}
 	render() {
-		let cls = classNames("table", {
-			hoverable: this.props.hoverable
-		});
 
 		let data = this.getItemsByCriteria();
 
 		let bodyItems = data.map((row: any, i)=>{
-			return <tr key={i}>{
+			return <tr onClick={()=>this.props.rowOnClick?this.props.rowOnClick(row):null} key={i}>{
 				this.props.headerItems.map((item, i)=>{
 					if (this.props.columnUI && this.props.columnUI[item]) {
-						return <td key={item}>{this.props.columnUI[item](row[item])}</td>;
+						return <td key={item}>{this.props.columnUI[item](row)}</td>;
 					}
 					return <td key={item}>{row[item]}</td>
 				})
@@ -90,18 +106,42 @@ export class Table extends React.Component<IProps, IState> {
 			return <th key={item} onClick={()=>{
 				this.sortBy(item);
 			}}>
-				<Layout>
+				<Layout gutter={20}>
 					<Section remain>{item}</Section>
 					<Section style={{width: 30, textAlign: 'right'}}>
 						{_.includes(this.props.sortableItems, item)?<i className={
 							(sort.by==item)?"fa fa-sort-amount-"+sort.order:"fa fa-unsorted"}></i>
 						:null}
 					</Section>
+					<Section>
+						{_.includes(this.props.groupableItems, item)?
+							<i className="fa fa-reorder button" onClick={(e)=>{
+								this.groupBy(item);
+								e.stopPropagation();
+							}}></i>
+						:null}
+					</Section>
 				</Layout>
 			</th>;
 		});
 
-		return <table className={cls}>
+		let cls = classNames("table", {
+			hoverable: this.props.hoverable
+		});
+
+		
+		let groupItems = this.state.group.by?<Menu header={_.capitalize(this.state.group.by)}>
+			{Object.keys(_.groupBy(this.props.data, this.state.group.by)).map((item)=>{
+				let cls = classNames("item", {
+					active: this.state.group.value==item
+				});
+				return <div className={cls} style={{minWidth: 150}} onClick={()=>{
+					this.state.group.by?this.groupBy(this.state.group.by, item):null;
+				}}>{item}</div>;
+			})}
+		</Menu>:null;
+
+		let dataTable = <table className={cls}>
 			<THead>
 				{headerItems}
 			</THead>
@@ -109,6 +149,10 @@ export class Table extends React.Component<IProps, IState> {
 				{bodyItems}
 			</TBody>
 		</table>;
+		return this.state.group.by?<Layout gutter={20} align="start">
+			<Section>{groupItems}</Section>
+			<Section remain>{dataTable}</Section>
+		</Layout>:dataTable;
 	}
 }
 
