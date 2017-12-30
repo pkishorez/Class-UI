9a0773829b99b559718a39541103b5ab32f6e66d
Kishore <kishore.iiitn@gmail.com>
2017-12-30 15:35:10 +0530

    Style Select Component.

--- ts/Components/Form/Select.tsx
+++ ts/Components/Form/Select.tsx
@@ -3,14 +3,22 @@ import * as ReactDOM from 'react-dom';
 import * as propTypes from 'prop-types';
 import {FormElement} from './FormElement';
 import { IFormContextType } from './Form';
+import * as classNames from 'classnames';
+import { SAnim } from '../../Helper/Animation';
 
 export interface IProps {
 	name: string
 	options: string[]
 	children?: any
+	top?: boolean
+	nonEditable?: boolean
 };
 
-export class Select extends FormElement<IProps, any> {
+export interface IState {
+	value: string
+	showSuggestions: boolean
+}
+export class Select extends FormElement<IProps, IState> {
 	private select: HTMLSelectElement | null;
 	private defaultValue: string;
 
@@ -19,7 +27,14 @@ export class Select extends FormElement<IProps, any> {
 		context.initialize(props.name, this, (schema, defaultValue)=>{
 			// Ignore Schema for now.
 			this.defaultValue = defaultValue;
-		})
+		});
+		this.state = {
+			value: this.defaultValue,
+			showSuggestions: false
+		};
+		this.onChange = this.onChange.bind(this);
+		this.suggestions = this.suggestions.bind(this);
+		this.getSuggestions = this.getSuggestions.bind(this);
 	}
 
 	componentWillUnmount()
@@ -36,12 +51,70 @@ export class Select extends FormElement<IProps, any> {
 	public validate() {
 		// No Validation as of now.
 	}
-
+	private onChange(e: React.ChangeEvent<HTMLInputElement>) {
+		if (this.props.nonEditable) {
+			this.setState({
+				showSuggestions: true
+			});
+			return;
+		}
+		this.setState({
+			value: e.target.value,
+			showSuggestions: true
+		});
+	}
+	private suggestions(e: React.KeyboardEvent<HTMLInputElement>) {
+		// Suggestions and navigating the suggestions logic goes here... TODO
+		if (e.key=="ArrowDown") {
+			this.setState({
+				showSuggestions: true
+			});
+		}
+	}
+	private getSuggestions() {
+		if (this.props.nonEditable) {
+			return this.props.options;
+		}
+		return this.props.options.filter((option)=>{
+			if (option.toLowerCase().indexOf(this.state.value.toLowerCase())!=-1)
+				return option;
+		});
+	}
 	render() {
-		return <select defaultValue={this.defaultValue} name={this.props.name} ref={(ref)=>{this.select = ref;}}>
-			{this.props.options.map((option)=>{
-				return <option key={option} value={option}>{option}</option>
-			})}
-		</select>;
+		let cls = classNames("select", {
+			top: this.props.top,
+			editable: !this.props.nonEditable
+		})
+		return <label className={cls}>
+			<input spellCheck={false} autoComplete="off" type="text" onClick={()=>{
+				this.setState({
+					showSuggestions: true
+				})
+			}} className="input" onFocus={()=>{
+				this.setState({
+					showSuggestions: true
+				});
+			}} value={this.state.value}
+				name={this.props.name}
+				onKeyDown={this.suggestions}
+				onChange={this.onChange}
+				onBlur={()=>{
+				setTimeout(()=>this.setState({
+					showSuggestions: false
+				}), 100);
+			}}/>
+			<SAnim show={this.state.showSuggestions} animType={this.props.top?"slideTop":"slideBottom"}>
+				<div className="suggestions">
+					{this.getSuggestions().map((option)=>{
+						return <span key={option} className={(option==this.state.value)?"active":""} onClick={()=>{
+							this.setState({
+								value: option,
+								showSuggestions: false
+							})
+						}}>{option}</span>
+					})}
+				</div>
+			</SAnim>
+		</label>
 	}
 };
