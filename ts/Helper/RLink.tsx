11baeae60b4fda4c4349bfc8e4096504a537b6fe
Kishore <kishore.iiitn@gmail.com>
2017-12-30 15:34:04 +0530

    Modify RLink Component.

--- ts/Helper/RLink.tsx
+++ ts/Helper/RLink.tsx
@@ -1,22 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {withRouter} from 'react-router-dom';
+import { RouteComponentProps } from 'react-router';
 
 export interface IProps {
-	history?: any
 	to?: string
 	children: React.ReactElement<any>
 };
 export interface IState {};
 
-let _RLink = (props: IProps) => {
-	return props.to?React.cloneElement(props.children, {
-		onClick: (e: any)=>{
-			if (props.children.props.onClick)
-				props.children.props.onClick(e)
-			props.history.push(props.to)
-		}
-	}):props.children;
+let _RLink = (props: IProps & RouteComponentProps<any>) => {
+	if (props.to) {
+		return React.cloneElement(props.children, {
+			onClick: (e: any)=>{
+				if (props.children.props.onClick)
+					props.children.props.onClick(e)
+				props.history.push(props.to as string)
+			}
+		});
+	}
+	return props.children;
 }
 
-export let RLink = withRouter<IProps>(_RLink);
+export let RLink = withRouter<IProps & RouteComponentProps<any>>(_RLink);
