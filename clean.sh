46015fc5627fa3b41f281bb3055566477a75691b
Kishore <kishore@Kishores-MacBook-Pro.local>
2018-08-07 17:22:34 +0530

    set allowSyntheticDefaultImports to true to avoid weird errors from third party.

---
+++ clean.sh
@@ -0,0 +1,14 @@
+# !/bin/sh
+
+rm -r ./Components
+rm -r ./DataStructures
+rm -r ./Helper
+rm -r ./docs
+rm -r ./Emotion
+rm -r ./Overlay
+rm ./ClassUI.d.ts
+rm ./ClassUI.js
+rm ./Navbar.d.ts
+rm ./Navbar.js
+rm ./index.js
+rm ./index.d.ts
--- publish.sh
+++ publish.sh
@@ -1,33 +1,10 @@
 # !/bin/sh
 
-rm -r ./Components
-rm -r ./DataStructures
-rm -r ./Helper
-rm -r ./docs
-rm -r ./Emotion
-rm -r ./Overlay
-rm ./ClassUI.d.ts
-rm ./ClassUI.js
-rm ./Navbar.d.ts
-rm ./Navbar.js
-rm ./index.js
-rm ./index.d.ts
-
+./clean.sh
 npm run compile
 
 npm version patch -m "patch"
 
 npm publish
 
-rm -r ./Components
-rm -r ./DataStructures
-rm -r ./Helper
-rm -r ./docs
-rm -r ./Emotion
-rm -r ./Overlay
-rm ./ClassUI.d.ts
-rm ./ClassUI.js
-rm ./Navbar.d.ts
-rm ./Navbar.js
-rm ./index.js
-rm ./index.d.ts
+./clean.sh
