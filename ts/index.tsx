e124532c2e6d659cba5c7df63684d20a3c1e6d02
Kishore <kishore.p@zen3.com>
2018-08-24 21:24:56 +0530

    Remove font awesome. Add material icons on fly.

--- ts/index.tsx
+++ ts/index.tsx
@@ -1,3 +1,11 @@
 export { ClassUI } from "classui/ClassUI";
 export { NavBar, NavbarRemain } from "classui/Navbar";
 export { Content } from "classui//Content";
+
+// Import Material font to head tag here itself!
+const link = document.createElement("link");
+link.type = "text/css";
+link.rel = "stylesheet";
+link.href = "https://fonts.googleapis.com/icon?family=Material+Icons";
+
+document.head.appendChild(link);
