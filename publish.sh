b4689dd79a5811f2f46c964e1f8077cf674963f9
Kishore <kishore@Kishores-MacBook-Pro.local>
2018-10-10 21:53:34 +0530

    update publish script.

--- publish.sh
+++ publish.sh
@@ -6,10 +6,10 @@ cp package.json ./dist/package.json
 cp -r ./assets/ ./dist/
 cd dist
 echo $PWD
-# npm version patch -m "patch"
+npm version patch -m "patch"
 cp package.json ../package.json
 
-# npm publish
+npm publish
 cd ../
 
 # Delete if you don't want dist folder.
